7a5598e478357abb78aeee46caa6562ab45644f6
Iván <ivantbd87@gmail.com>
2023-07-14 19:03:04 +0200

    dices app done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados';
+  dadoIzda = "../assets/img/dice1.png"
+  dadoDcha = "../assets/img/dice2.png"
+
+  numero1: number | undefined = 1
+  numero2: number | undefined = 2
+
+  tirarDados(): void {
+    this.numero1 = Math.floor((Math.random()* 6) + 1)
+    this.numero2 = Math.floor((Math.random()* 6) + 1)
+    this.dadoIzda = `../assets/img/dice${this.numero1}.png`
+    this.dadoDcha = `../assets/img/dice${this.numero2}.png`
+  }
 }
